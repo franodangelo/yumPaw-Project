132580d30fa1b81e428e03ff00381e0ba0f36fa7
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-07-29 18:55:56 -0300

    changes in admin profile

--- client/src/Views/Profile/AdminProfile.jsx
+++ client/src/Views/Profile/AdminProfile.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
-import NavBarShop from "../../Components/NavBar/NavBarShop";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-import Footer from "../../Components/Footer/Footer";
-import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+import NavBarShop from "../../Components/NavBar/NavBarShop";
+import Footer from "../../Components/Footer/Footer";
 import styleContainer from "../../Components/GlobalCss/InContainer.module.css";
 import style from "./Profile.module.css";
 
 export default function Profile() {
-  const pets = useSelector((state) => state.pets);
   const dispatch = useDispatch();
   const [userData, setUser] = useState({});
   const { user, isAuthenticated } = useAuth0();
@@ -18,9 +17,8 @@ export default function Profile() {
     if (isAuthenticated) {
       axios
         .get("https://proyecto-grupal.herokuapp.com/owners")
-        .then((x) => {
-          const userdb = x.data.find((x) => x.email === user.email);
-          console.log(userdb);
+        .then(au => {
+          const userdb = au.data.find(u => u.email === user.email);
           setUser({
             nombre: user.name,
             picture:
@@ -30,9 +28,8 @@ export default function Profile() {
             email: user.email,
             pets: userdb ? userdb.pets : [],
             address: userdb.address,
-            isAdmin: userdb.isAdmin,
+            isAdmin: userdb.isAdmin
           });
-          console.log("userdb", userdb);
         })
         .then(() => {
           return axios.get("https://proyecto-grupal.herokuapp.com/events");
@@ -45,7 +42,7 @@ export default function Profile() {
       <NavBarShop />
       <div className={styleContainer.container}>
         <section className={style.infoProfile}>
-          <img src={userData.picture} alt="profilePicture" />
+          <img src={userData.picture} alt="profilePicture"/>
           <article className={style.profile}>
             <h1 className={style.name}>{user.name}</h1>
             <div>
@@ -57,42 +54,31 @@ export default function Profile() {
         </section>
         <section className={style.mainInfoProfile}>
           <h2>Mis datos</h2>
-          <h4 className={style.email}>
-            {" "}
-            Correo electronico: <span className={style.span}>{user.email}</span>
-          </h4>
+          <h4 className={style.email}>{" "}Correo electronico: <span className={style.span}>{user.email}</span></h4>
           <h4 className={style.address}>
             Direccion:{" "}
-            <span className={style.span}>
-              {userData.address ? userData.address.road : null}
-            </span>{" "}
+            <span className={style.span}>{userData.address ? userData.address.road : null}</span>
           </h4>
         </section>
-        <br />
-        <br />
+        <br/>
+        <br/>
         <section>
           {userData.isAdmin ? (
             <div className={style.adminPanel}>
               <Link to="/admin/listado-productos">
-                <button className="primaryButton">
-                  Ver listado de productos en Petshop
-                </button>
+                <button className="primaryButton">Ver listado de productos en Petshop</button>
               </Link>
               <Link to="/admin/ventas-petshop">
-                <button className="primaryButton">
-                  Ver listado de comprobantes de compra
-                </button>
+                <button className="primaryButton">Ver listado de comprobantes de compra</button>
               </Link>
               <Link to="/admin/get-users">
-                <button className="primaryButton">
-                  Ver listado de usuarios registrados
-                </button>
+                <button className="primaryButton">Ver listado de usuarios registrados</button>
               </Link>
             </div>
           ) : null}
         </section>
       </div>
-      <Footer />
+      <Footer/>
     </main>
   );
-}
+};
