eb24d460bbb36ef693be3c5d87b9de7c688cd0f1
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-07-30 12:03:19 -0300

    cleaned up files

--- client/src/Components/Auth0/NotRegistered.jsx
+++ client/src/Components/Auth0/NotRegistered.jsx
@@ -1,29 +1,16 @@
-import React, { useEffect } from "react";
-import Swal from "sweetalert2";
-import Footer from "../Footer/Footer";
-import NavBar from "../NavBar/NavBarShop";
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./NotRegistered.module.css";
 import InContainer from "../GlobalCss/InContainer.module.css";
-import { Link } from "react-router-dom";
 
-export const NotRegistered = () => {
-  //   useEffect(() => {
-  //     Swal.fire({
-  //       icon: "error",
-  //       title: "Oops...",
-  //       text: "Para continuar debes iniciar sesión",
-  //       footer: '<a href="">Why do I have this issue?</a>',
-  //     });
-  //   }, []);
+export default function NotRegistered() {
   return (
     <div>
       <div className={InContainer.container}>
         <div className={styles.container}>
           <img src="./assets/img/not-registered.png" alt="" className={styles.img} />
           <div className={styles.log}>
-            <h1 className={styles.text}>
-              ¡OOPS! <br /> Para continuar tenés que iniciar sesión
-            </h1>
+            <h1 className={styles.text}>¡OOPS! <br /> Para continuar tenés que iniciar sesión</h1>
             <Link to='/'>
               <button className='primaryButton'>Ir al inicio</button>
             </Link>
@@ -33,5 +20,3 @@ export const NotRegistered = () => {
     </div>
   );
 };
-
-export default NotRegistered;
