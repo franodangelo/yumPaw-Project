c331ed803bdad051cf0b9c98d5c47da8ab3d518e
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-01 11:25:36 -0300

    cleaned up shop mp files

--- client/src/Components/Shop/MercadoPago/PurchaseConfirmation.jsx
+++ client/src/Components/Shop/MercadoPago/PurchaseConfirmation.jsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import MercadoPago from "./MercadoPago";
 import { chargeCart } from "../../../redux/actions/petshopActions";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-import InContainer from "../../GlobalCss/InContainer.module.css";
-import PurchaseDetail from "./PurchaseDetail";
 import NavBar from "../../NavBar/NavBarShop";
+import PurchaseDetail from "./PurchaseDetail";
 import Footer from "../../Footer/Footer";
+import InContainer from "../../GlobalCss/InContainer.module.css";
 import styles from "./PurchaseConfirmation.module.css";
 
 export default function PurchaseConfirmation() {
-  const { user, isAuthenticated } = useAuth0();
-
-  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+  const { user, isAuthenticated } = useAuth0();
   const [total, setTotal] = useState(0);
+  const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
     if (user) {
@@ -24,13 +23,12 @@ export default function PurchaseConfirmation() {
   }, [dispatch, user]);
 
   useEffect(() => {
-    var suma = 0;
+    var addition = 0;
     if (cart && cart.length) {
       cart.forEach((x) => {
-        suma += x.price * x.quantity;
-        console.log("TOTAL", total);
+        addition += x.price * x.quantity;
       });
-      setTotal(suma);
+      setTotal(addition);
     } else {
       setTotal(0);
     }
@@ -40,7 +38,6 @@ export default function PurchaseConfirmation() {
     <div>
       <NavBar />
       <div className={InContainer.container}>
-        {/* <img src="./assets/img/arrow-left.svg" alt="" /> */}
         <div className={styles.container}>
           <h2 className={styles.title}>Detalle de tu compra</h2>
           {cart && cart.length > 0 ? (
@@ -59,20 +56,20 @@ export default function PurchaseConfirmation() {
             <h1>No hay ningún producto en el carrito</h1>
           )}
           <div className={styles.compra}>
-            <h4 className={styles.total}>TOTAL COMPRA: ${total}</h4>
+            <h4 className={styles.total}>Monto total: ${total}</h4>
           </div>
-          <h2 className={styles.continuar}>Continuá con el pago</h2>
+          <h2 className={styles.continuar}>Continuá con tu pago</h2>
           <div className={styles.buttonFlex}>
             <div className={styles.pagar}>
-            <MercadoPago cart={cart} />
+              <MercadoPago cart={cart} />
             </div>
           </div>
         </div>
         <Link to="/mi-carrito">
-              <button className="secondaryButton">Volver al carrito</button>
-            </Link>
+          <button className="secondaryButton">Volver al carrito</button>
+        </Link>
       </div>
       <Footer />
     </div>
   );
-}
+};
